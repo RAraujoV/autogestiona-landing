ef325b2f18e0c94854f491a87ebb31e66e177553
unknown <Administrator@ETPC0155.etsasal.local>
2025-12-06 19:22:40 +0100

    fix: Final check on Tailwind content array for production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    // La clave es que detecte archivos .html, .js, .jsx, etc.
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
